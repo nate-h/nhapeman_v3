aa7493496b41a8bec4bb5bdf51c1896f777b2a5b
nate <nhapeman@gmail.com>
2019-02-09 18:49:58 -0800

    Start projects stuff.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,29 @@
 <template>
-    <div class="home">
+  <div class="home">
+    <about-me-container></about-me-container>
 
-        <about-me-container></about-me-container>
-
-        <header-container header="Job History" class=job-components>
-            <!-- Inject Job Components -->
-            <job-container
-              company='Applied Invention'
-              :img="require('img/ai.png')"
-              link='https://www.appliedinvention.com/'
-              years='June 2014-Present'
-              short-description='Software Engineer'>
-
-                This is my current full time job and an amazing company! At Applied Invention, we work on creating innovative solutions for leading companies and entrepreneurs. All of the projects I've worked on have been extremely interesting and exciting to work on. The longest project I worked on was our in-house geo-spacial game engine that was built for simulations. The engine was written in c++ and used panda3d, an open source 3D game engine. Currently, I am working as a front-end engineer on an in-house website used to collect training data for machine learning algorithms.
-            </job-container>
-            <job-container
-              company='FrackOptima'
-              :img="require('img/frackoptima.png')"
-              link='http://frackoptima.com/'
-              years='May 2016-Present'
-              short-description='Software Engineer'>
-                I worked part-time at FrackOptima as a software engineer while working full-time for Applied Invention. FrackOptima is a hydraulic fracturing company that specializes in software and services for optimal hydraulic fracturing treatments. There's several systems which make this all possible. Most of my work has been on the company website and the fracking simulator.
-            </job-container>
-        </header-container>
-
-    </div>
+    <header-container header="Job History" class="job-components">
+      <!-- Inject Job Components -->
+      <job-container
+        company="Applied Invention"
+        :img="require('img/ai.png')"
+        link="https://www.appliedinvention.com/"
+        years="June 2014-Present"
+        short-description="Software Engineer"
+      >
+        <p>This is my current full time job and an amazing company! At Applied Invention, we work on creating innovative solutions for leading companies and entrepreneurs. All of the projects I've worked on have been extremely interesting and exciting to work on. The longest project I worked on was our in-house geo-spacial game engine that was built for simulations. The engine was written in c++ and used panda3d, an open source 3D game engine. Currently, I am working as a front-end engineer on an in-house website used to collect training data for machine learning algorithms.</p>
+      </job-container>
+      <job-container
+        company="FrackOptima"
+        :img="require('img/frackoptima.png')"
+        link="http://frackoptima.com/"
+        years="May 2016-Present"
+        short-description="Software Engineer"
+      >
+        <p>I worked part-time at FrackOptima as a software engineer while working full-time for Applied Invention. FrackOptima is a hydraulic fracturing company that specializes in software and services for optimal hydraulic fracturing treatments. There's several systems which make this all possible. Most of my work has been on the company website and the fracking simulator.</p>
+      </job-container>
+    </header-container>
+  </div>
 </template>
 
 <script>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="projects">
-    <about-me-container></about-me-container>
+    <header-container header="Projects" class="job-components">
+        testing
+    </header-container>
   </div>
 </template>
 
 <script>
-import AboutMeContainer from "@/components/AboutMe.vue";
 export default {
     name: "Projects",
-    components: {
-        "about-me-container": AboutMeContainer
-    }
+    components: {}
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
 .projects {
     padding: $padding-large;
