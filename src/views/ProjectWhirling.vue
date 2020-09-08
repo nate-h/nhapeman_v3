61329b071aa6de7c8d1b8f363918778203641b30
nates linux desktop <nhapeman@gmail.com>
2020-09-08 11:33:14 -0700

    Add more structure to code

--- src/views/ProjectWhirling.vue
+++ src/views/ProjectWhirling.vue
@@ -43,24 +43,41 @@
                 and 6 make heavy use of moving averages, thresholds and other
                 heuristics to get the colors to render just right. Visualizers 1
                 and 4->6 use the same colors to represent the separated source
-                that's heard. See color key below for color mappings. Brief
-                description of each visualizer: 1. combo_board: Uses both
-                spectrograms and features to render a grid of squares. Each
-                illuminated square represents a certain frequency range heard.
-                Squares near the top right of the screen map to higher
-                frequencies. The color of that square represents what source it
-                came from. 2. debug: Shows all features generated for current
-                source separation plan. 3. spectrogram: Shows all spectrograms
-                generated for current source separation plan. 4.
-                concentric_squares: Uses the source separated spectrograms to
-                render concentric square rings that represent what frequency is
-                being played. The bigger the ring, the higher the frequency.
-                Ring color matches source heard. This visualizer combines colors
-                if 2+ sources are playing the same frequency. 5.
-                stacked_equalizers: Show all frequencies heard with no
-                adjustments. 6. checkerboard: same as combo_board but with no
-                use of features and much less filtering.
+                that's heard. See color key below for color mappings.
             </p>
+            <p>Brief description of each visualizer:</p>
+            <ol>
+                <li>
+                    combo_board: Uses both
+            spectrograms and features to render a grid of squares. Each
+            illuminated square represents a certain frequency range heard.
+            Squares near the top right of the screen map to higher
+            frequencies. The color of that square represents what source it
+            came from.
+                </li>
+                <li>
+                    debug: Shows all features generated for current
+            source separation plan.
+                </li>
+                <li>
+                    spectrogram: Shows all spectrograms
+            generated for current source separation plan.
+                </li>
+                <li>
+                    concentric_squares: Uses the source separated spectrograms to
+            render concentric square rings that represent what frequency is
+            being played. The bigger the ring, the higher the frequency.
+            Ring color matches source heard. This visualizer combines colors
+            if 2+ sources are playing the same frequency.
+                </li>
+                <li>
+                    stacked_equalizers: Show all frequencies heard with no
+            adjustments.
+                </li>
+                <li>
+                    checkerboard: same as combo_board but with no use of features and much less filtering.
+                </li>
+            </ol>
             <h2>TLDR - I built a music visualizer</h2>
             <p>
                 Code
