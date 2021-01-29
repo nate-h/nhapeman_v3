36ce794edfdfae0a3dcf695d7818bd22a29aa982
nates linux desktop <nhapeman@gmail.com>
2021-01-29 00:12:49 -0800

    Have google do the pdf rendering.

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -2,16 +2,16 @@
     <div class="resume">
         <!-- <embed
             class="pdf"
-            :src="require('subs/resume/Nathanial.Hapeman.Resume.pdf')"
-            type="application/pdf"
-        /> -->
-
-        <iframe
-            id="ifamePdf"
             type="application/pdf"
             scrolling="auto"
+            frameBorder="0"
             :src="require('subs/resume/Nathanial.Hapeman.Resume.pdf')"
-            width="100%"
+        /> -->
+
+        <iframe src="https://drive.google.com/file/d/1EITc-fPHTZMSd7i7PxE6OetYMn7fFuMq/preview"
+            class="pdf"
+            frameBorder="0"
+            scrolling="auto"
         ></iframe>
     </div>
 </template>
